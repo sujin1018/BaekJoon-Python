dd194e5a204af0cf26be953f791a3d6d6886ad33
박수진 <64060916+sujin1018@users.noreply.github.com>
2023-03-21 16:28:18 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,31 @@
+import sys
+input=sys.stdin.readline
+n=int(input())
+s=[]
+for i in range(n):
+    args=input().split()
+    if(len(args)!=1):
+        a,b=args[0],int(args[1])
+        if(a=='push'):
+            s.append(b)
+        continue
+    else:
+        a=args[0]
+        if(a=='pop'):
+            if(len(s)!=0):
+                print(s[-1])
+                s.pop()
+            else:
+                print(-1)
+        elif(a=='size'):
+            print(len(s))
+        elif(a=='empty'):
+            if(len(s)==0):
+                print(1)
+            else:
+                print(0)
+        elif(a=='top'):
+            if(len(s)==0):
+                print(-1)
+            else:
+                print(s[-1])
