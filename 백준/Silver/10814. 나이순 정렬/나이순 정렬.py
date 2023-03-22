e7c070baeb7ad72be63ac2b42e274e0cf44ed7ff
박수진 <64060916+sujin1018@users.noreply.github.com>
2023-03-22 16:44:06 +0900

    [Silver V] Title: 나이순 정렬, Time: 244 ms, Memory: 48324 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,10 @@
+import sys
+input=sys.stdin.readline
+n=int(input())
+a=[]
+for _ in range(n):
+    age,name=input().split()
+    a.append([int(age),name])
+a.sort(key=lambda x:x[0])
+for i in a:
+    print(i[0], i[1])
