a05b6913b2c40919a421789567a4c5e20972065a
박수진 <64060916+sujin1018@users.noreply.github.com>
2023-03-25 12:17:54 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9204 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,11 @@
+import sys
+input=sys.stdin.readline
+n=int(input())
+a=[0]*10001
+for i in range(n):
+    s=int(input())
+    a[s]+=1
+for i in range(10001):
+    if a[i]!=0:
+        for j in range(a[i]):
+            print(i)
