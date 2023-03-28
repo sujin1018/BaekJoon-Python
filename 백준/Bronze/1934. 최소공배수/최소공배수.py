eec05d4cd806cf1536af05adabaa08644e8dc9c6
박수진 <64060916+sujin1018@users.noreply.github.com>
2023-03-28 21:00:14 +0900

    [Bronze I] Title: 최소공배수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,12 @@
+import sys
+input=sys.stdin.readline
+n=int(input())
+def gcd(a,b):
+    while b>0:
+        a,b=b,a%b
+    return a
+for i in range(n):
+    a,b=map(int,input().split())
+
+    print(a*b//gcd(a,b))
+        
