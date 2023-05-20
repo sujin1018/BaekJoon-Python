9831227e156915df9fdef6c9c669d2f918a6048c
박수진 <64060916+sujin1018@users.noreply.github.com>
2023-05-20 17:30:46 +0900

    [Bronze III] Title: ACM 호텔, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,11 @@
+import sys
+input=sys.stdin.readline
+n=int(input())
+for i in range(n):
+    h,w,n=map(int,input().split())
+    f=n%h
+    r=n//h+1
+    if f==0:
+        f=h
+        r-=1
+    print(f*100+r)
