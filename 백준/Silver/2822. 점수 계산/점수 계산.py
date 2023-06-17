67ef71a3fb254f4c59d26bbcf64bf8eec8a93b67
박수진 <64060916+sujin1018@users.noreply.github.com>
2023-06-17 21:04:42 +0900

    [Silver V] Title: 점수 계산, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2822. 점수 계산/점수 계산.py
+++ 백준/Silver/2822. 점수 계산/점수 계산.py
@@ -1,13 +1,8 @@
-d={}
-s=0
-a,b=[],[]
-for i in range(1,9):
-    d[i]=int(input())
-res=sorted(d.items(), key=lambda x:x[1])
-for k,v in res:
-   a.append(v)
-   b.append(k)
-print(sum(a)-a[0]-a[1]-a[2])
-b=b[3:8]
-for i in sorted(b):
-    print(i,end=' ')
+import sys
+input=sys.stdin.readline
+a=[int(input()) for _ in range(8)]
+res=sorted(a,reverse=True)
+b=[a.index(i)+1 for i in res[:5]]
+b.sort()
+print(sum(res[:5]))
+print(*b)
