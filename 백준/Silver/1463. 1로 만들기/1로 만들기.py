4be71c6ec36031d4773f2839812e8595830f03a9
박수진 <64060916+sujin1018@users.noreply.github.com>
2023-07-24 11:21:42 +0900

    [Silver III] Title: 1로 만들기, Time: 560 ms, Memory: 39068 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,9 @@
+n=int(input())
+a=[0]*(n+1)
+for i in range(2,n+1):
+    a[i]=a[i-1]+1
+    if i%3==0:
+        a[i]=min(a[i],a[i//3]+1)
+    if i%2==0:
+        a[i]=min(a[i],a[i//2]+1)
+print(a[n])
