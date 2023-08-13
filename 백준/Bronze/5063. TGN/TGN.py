85319e14f1d1cea2337963c389ce7342f81f280e
박수진 <64060916+sujin1018@users.noreply.github.com>
2023-08-13 22:17:01 +0900

    [Bronze III] Title: TGN, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,10 @@
+import sys
+input=sys.stdin.readline
+for _ in range(int(input())):
+    r,e,c=map(int,input().split())
+    if r==(e-c):
+        print("does not matter")
+    elif r>(e-c):
+        print("do not advertise")
+    else:
+        print("advertise")
