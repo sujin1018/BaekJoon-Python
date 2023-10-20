819b2ac7a12e317574c4e78debec096f4e9fc02e
박수진 <64060916+sujin1018@users.noreply.github.com>
2023-10-20 22:02:47 +0900

    [Silver I] Title: 팔, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1105. 팔/팔.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+l,r=input().split()
+res=0
+if len(l)!=len(r):
+    print(0)
+else:
+    for i in range(len(l)):
+        if l[i]==r[i]:
+            if l[i]=='8':
+                res+=1
+        else:   
+            break
+    print(res)
